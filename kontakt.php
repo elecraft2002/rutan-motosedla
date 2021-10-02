0165783d2de0a6d9ce4ef08401b00863a4e4ebbd
Vojtěch Suchánek <cenenaci.cz@gmail.com>
2021-10-02 22:16:33 +0200

    main

--- kontakt.php
+++ kontakt.php
@@ -5,16 +5,48 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Rutan Performance Kontakt</title>
     <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
     <link rel="stylesheet" href="./styles/nav.css">
     <link rel="stylesheet" href="./styles/gallery.css">
+    <link rel="stylesheet" href="./styles/kontakt.css">
     <script src="https://kit.fontawesome.com/7a91434f52.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
     <?php include __DIR__ . "/php/templates/nav.php"
     ?>
+    <main>
+        <div class="doba">
+            <h2>Betriebszeiten:</h2>
+            <p><span>MON-FRI: 9-15h</span> !!! IMMER NACH VORHERIGER TELEFONISCHER VEREINBARUNG</p>
+        </div>
+        <div>
+            <h2>Wo Sie uns finden</h2>
+            <div class="info">
+                <div class=" info--left">
+                    <p>RUTAN ACCESSORIES s.r.o.</p>
+                    <p>OPERATIONEN</p>
+                    <p>Mníšecká 107</p>
+                    <p>463 31 Mníšek _ Fojtka</p>
+                </div>
+                <div class="info--right">
+                    <p>Telefon:<span> <a href="tel:+420 735 757 027">+420 735 757 027</a></span></p>
+                    <p><span><a href="tel:+420 733 612 642">+420 733 612 642</a></spa>
+                    </p>
+                    <p>E-mail:<span> <a href="mailto:info@rutan.cz">info@rutan.cz</a></span></p>
+                </div>
+            </div>
+            <iframe class="map" style="border: 0;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2520.4827988318425!2d15.059820515906203!3d50.822220568387735!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4709319ad191b671%3A0x383276cf56edf86c!2sRUTAN+Performance!5e0!3m2!1scs!2scz!4v1550580254084" width="600" height="450" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+            <p>Bestellvorgang - senden Sie uns eine Anfrage mit dem Motorradtyp an <span> <a href="mailto:info@rutan.cz"></a>info@rutan.cz</a></span> oder verwenden Sie das
+                untenstehende Kontaktformular. Geben Sie an, welche Änderungen Sie wünschen, und wir senden Ihnen ein
+                Angebot mit Fotos und weiteren Informationen.
+
+                <br>Dann schicken Sie uns den Sattel, wir wählen gemeinsam eine Designkonfiguration aus und haben ihn
+                innerhalb von zwei Wochen zurück!
+            </p>
+        </div>
+    </main>
 </body>
 
 </html>
